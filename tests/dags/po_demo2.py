d0809b300f96e2f61d44d60ddc7c76bc23acafe5
Sablin Igor <sablin.igor@gmail.com>
2023-03-03 15:20:08 +0100

    Another DAG

---
+++ tests/dags/po_demo2.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+import time
+
+def my_func():
+    time.sleep(60)
+    print('Hello from my_func')
+
+with DAG('python_dag2', description='Python DAG', schedule_interval='*/5 * * * *', start_date=datetime(2018, 11, 1), catchup=False) as dag:
+    dummy_task 	= DummyOperator(task_id='dummy_task', retries=3)
+    python_task	= PythonOperator(task_id='python_task', python_callable=my_func)
+
+    dummy_task >> python_task
